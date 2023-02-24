394b7237151da3542931e5d9e527821a733cbc67
Jacques Gomes <113613459+JacquesGomes@users.noreply.github.com>
2023-02-24 15:08:23 -0300

    Small changes

--- Timer-90_minutes/Timer_bell.py
+++ Timer-90_minutes/Timer_bell.py
@@ -10,10 +10,10 @@ def play_bell():
     global timer
     # Generate a interval of 90 minutes
     interval = (5400)
-    # Schedule the next beep sound to play after the interval
+    # Schedule the next bell sound to play after the interval
     timer = threading.Timer(interval, play_bell)
     timer.start()
-    # Play the beep sound
+    # Play the bell sound
     play(bell_sound)
 
 def main():
@@ -21,7 +21,7 @@ def main():
     play_bell()
     # Wait for the user to press enter to stop the program
     input("Press Enter to stop...")
-    # Stop the timer for the next beep sound
+    # Stop the timer for the next bell sound
     timer.cancel()
 
 if __name__ == "__main__":
